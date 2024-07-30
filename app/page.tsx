1cd27556dbce56c13e33c2b046d4825bb29ef223
KasunViranga1 <kasunviranga12@gmail.com>
2024-07-30 12:50:41 +0530

    fix bug in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative flex flex-col items-center justify-center overflow-hidden bg-black-100 px-5 sm:px-10">
+    <main className="relative flex flex-col items-center justify-center bg-black-100 px-5 sm:px-10">
       <div className="w-full max-w-7xl">
         <FloatingNav navItems={navItems} />
         <Hero />
